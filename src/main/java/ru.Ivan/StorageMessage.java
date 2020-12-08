137a2b910180bf36c0f283c28de8330d6408b154
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 16:23:36 +0300

    Scripted auto-commit on change (2020-12-08 16:23:36) by gitwatch.sh

--- src/main/java/ru.Ivan/StorageMessage.java
+++ src/main/java/ru.Ivan/StorageMessage.java
@@ -1,4 +1,19 @@
 package ru.Ivan;
 
 public class StorageMessage {
+    private String url;
+    private Integer time;
+
+    public StorageMessage(String url, Integer time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public Integer getTime() {
+        return this.time;
+    }
 }
