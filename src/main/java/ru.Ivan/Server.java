229fc65910214cd3a450cbe2aab43143e384ee07
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 15:32:57 +0300

    Scripted auto-commit on change (2020-12-08 15:32:57) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -2,7 +2,8 @@ package ru.Ivan;
 
 public class Server {
     public static void main(String[] args) {
-        
+        System.out.println("Start!");
+        ActorSystem system = ActorSystem.create("routes");
 
     }
 }
