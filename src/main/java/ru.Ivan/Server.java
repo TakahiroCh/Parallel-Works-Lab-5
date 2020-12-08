988f8f63cfaf3e169750d1a642c23afccb81b106
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 15:34:16 +0300

    Scripted auto-commit on change (2020-12-08 15:34:16) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -1,9 +1,12 @@
 package ru.Ivan;
 
+import akka.actor.ActorSystem;
+
 public class Server {
     public static void main(String[] args) {
         System.out.println("Start!");
         ActorSystem system = ActorSystem.create("routes");
+        
 
     }
 }
