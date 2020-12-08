2dd928ba4901d52f93b36631754720cab1bf34c9
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 15:37:19 +0300

    Scripted auto-commit on change (2020-12-08 15:37:19) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -1,11 +1,20 @@
 package ru.Ivan;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class Server {
     public static void main(String[] args) {
         System.out.println("Start!");
         ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFLow = createFlow(http, system, materializer);
         
 
     }
